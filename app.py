b3590e90bef8e052857c2b4c16dfb7be0c05c5d1
SafarwithAK <153210619+SafarwithAK@users.noreply.github.com>
2026-04-15 11:13:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -23,8 +23,9 @@ except:
 
 # Models (stable ones)
 MODELS = [
-    "gemini-1.5-flash",
-    "gemini-1.5-pro"
+    "gemini-2.5-flash",
+    "gemini-2.0-flash",
+    "gemini-flash-lite-latest"
 ]
 
 # ======================
